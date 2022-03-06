0d1e39c447b45d1e2e9c2ace8e1d20e149174258
proshin <sdy8603@hanmail.net>
2022-03-07 00:32:50 +0900

    seqeulize적용, api 구조, 샘플 API 커밋

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors'); // 웹 에러 처리
+var express = require('express'); // nodejs express 프레임워크
+var path = require('path'); // path 경로
+var cookieParser = require('cookie-parser'); // cookie 파서
+var bodyParser = require('body-parser'); //body 파서
+var logger = require('morgan'); // express 로그
+// models sequelize 처리
+const { sequelize } = require('./models');
+// 테이블 
+sequelize
+.sync()
+.then(()=>{
+  console.log(" DB Connection success.");
+})
+.catch((err) => {
+  console.error(err);
+  console.log(" DB Connection error. Please make sure DB is running.");
+  process.exit();
+}); 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index'); //routes index.js
+var usersRouter = require('./routes/user'); //
 
-var app = express();
+var app = express(); // express 선언
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views')); // express views 
+app.set('view engine', 'pug'); // api 서버에서 필요없음
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
---
+++ controllers/user.js
@@ -0,0 +1,109 @@
+const db = require("../models")
+const User = db.User;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new user
+exports.create = (req, res, next) => {
+    // Validate request
+  if (!req.body.user_cd || !req.body.user_id) {
+    res.status(400).send({
+      message: "parameter is validation error!"
+    });
+    return;
+  }
+  // Create a Tutorial
+  const user = {
+    user_cd: req.body.user_cd,
+    user_id: req.body.user_id,
+    user_nm: req.body.user_nm
+  };
+  // Save Tutorial in the database
+  User.create(user)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the User."
+      });
+    });
+};
+// Retrieve all user from the database.
+exports.findAll = (req, res) => {
+  User.findAll()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
+};
+// Find a single user with an id
+exports.findOne = (req, res) => {
+  const user_cd = req.params.user_cd;
+  User.findByPk(user_cd)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Tutorial with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Tutorial with id=" + id
+      });
+    });
+};
+// Update a user by the id in the request
+exports.update = (req, res) => {
+  const user_cd = req.params.user_cd;
+  User.update(req.body, {
+    where: { user_cd: user_cd }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "User was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update User with user_cd=${user_cd}. Maybe User was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating User with user_cd=" + user_cd
+      });
+    });
+};
+// Delete a user with the specified id in the request
+exports.delete = (req, res) => {
+  const user_cd = req.params.user_cd;
+  User.destroy({
+    where: { user_cd: user_cd }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "User was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete User with user_cd=${user_cd}. Maybe User was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete User with user_cd=" + user_cd
+      });
+    });
+};
