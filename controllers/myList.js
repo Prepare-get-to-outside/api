91a91c682838d826f05547603245db69e808bf1c
jaen <beckyi@naver.com>
2022-03-27 15:08:14 +0900

    :zap: feat: Sequelize Managed transactions 추가

--- controllers/myList.js
+++ controllers/myList.js
@@ -1,31 +1,53 @@
+const {sequelize} = require("./../models")
 const db = require("../models")
 const MyList = db.MyList;
+const Restaurant = db.Restaurant;
 
 // Create and Save a new user
-exports.create = (req, res, next) => {
+exports.create = async (req, res, next) => {
+  try {
     // Validate request
-  if (!req.body.user_cd || !req.body.rest_cd) {
-    res.status(400).send({
-      message: "parameter is validation error!"
-    });
-    return;
-  }
-  // Create a Tutorial
-  const my_list = {
-    user_cd: req.body.user_cd,
-    rest_cd: req.body.rest_cd,
-  };
-  // Save Tutorial in the database
-  MyList.create(my_list)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the MyList."
+    if (!req.body.user_cd || !req.body.rest_cd) {
+      res.status(400).send({
+        message: "parameter is validation error!"
       });
+      return;
+    }
+    const { user_cd, rest_cd } = req.body
+    
+    // (Managed) Transactions 
+    await sequelize.transaction(async (t) => {
+ 
+      //// 이 쿼리를 트랜잭션 처리
+      const isRestaurant = await Restaurant.findByPk(rest_cd, { transaction: t });
+      
+      // Create a Tutorial
+      const my_list = {
+        user_cd,
+        rest_cd: isRestaurant.rest_cd,
+      };
+      
+      // Save Tutorial in the database
+      const response = await MyList.create(my_list, {
+        transaction: t, // 이 쿼리를 트랜잭션 처리
+      })
+
+      if (response)
+        res.send(response); 
+      else
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the MyList."
+        });
     });
+    // 트랜잭션 문제없으면 알아서 자동으로 커밋
+  } catch (err) {
+     // 중간에 failed 나면 알아서 자동으로 트랜잭션 롤백
+     res.status(500).send({
+      message:
+        err.message || "Some error occurred while creating the MyList."
+    });
+  }
 };
 
 // Retrieve all from the database.
